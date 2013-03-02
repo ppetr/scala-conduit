7d36896c7a58ebd6d4ab3b001bb39d1b9b1e3ce5
Petr <petr.mvd@gmail.com>
2013-03-02 23:12:43 +0100

    Added `Delay` instance, minor improvements.

--- Conduit.scala
+++ Conduit.scala
@@ -3,10 +3,15 @@ import annotation.tailrec
 sealed trait Pipe[I,O,R]
 {
   def flatMap[R1](f: R => Pipe[I,O,R1]): Pipe[I,O,R1];
+  final def >>[R1](p: => Pipe[I,O,R1]): Pipe[I,O,R1] = flatMap(_ => p);
   def map[R1](f: R => R1): Pipe[I,O,R1];
 
   final def >->[X](that: Pipe[O,X,R]) = Pipe.pipe(this, that);
   final def <-<[X](that: Pipe[X,I,R]) = Pipe.pipe(that, this);
+
+  final def forever[R]: Pipe[I,O,R] = Pipe.forever(this);
+
+  implicit def extend[I1 <: I,O1 >: O]: Pipe[I1,O1,R];
 }
 
 case class HaveOutput[I,O,R](output: O, next: () => Pipe[I,O,R])
@@ -16,6 +21,9 @@ case class HaveOutput[I,O,R](output: O, next: () => Pipe[I,O,R])
     HaveOutput(output, () => { next().flatMap(f) });
   def map[R1](f: R => R1): Pipe[I,O,R1] =
     HaveOutput(output, () => { next().map(f) });
+
+  def extend[I1 <: I,O1 >: O]: Pipe[I1,O1,R] =
+    HaveOutput(output, () => { next().extend });
 }
 
 case class NeedInput[I,O,R](consume: I => Pipe[I,O,R])
@@ -25,25 +33,50 @@ case class NeedInput[I,O,R](consume: I => Pipe[I,O,R])
     NeedInput(i => consume(i).flatMap(f));
   def map[R1](f: R => R1): Pipe[I,O,R1] =
     NeedInput(i => consume(i).map(f));
+
+  def extend[I1 <: I,O1 >: O]: Pipe[I1,O1,R] =
+    NeedInput(x => { consume(x).extend });
 }
 
 case class Done[I,O,R](result: R)
-    extends Pipe[I,O,R] {
+    extends Pipe[I,O,R]
+{
   def flatMap[R1](f: R => Pipe[I,O,R1]): Pipe[I,O,R1] =
     f(result);
   def map[R1](f: R => R1): Pipe[I,O,R1] =
     Done(f(result));
+
+  def extend[I1,O1]: Pipe[I1,O1,R] =
+    Done(result);
+}
+
+case class Delay[I,O,R](delayed: () => Pipe[I,O,R])
+    extends Pipe[I,O,R]
+{
+  def flatMap[R1](f: R => Pipe[I,O,R1]): Pipe[I,O,R1] =
+    Delay(() => delayed().flatMap(f));
+  def map[R1](f: R => R1): Pipe[I,O,R1] =
+    Delay(() => delayed().map(f));
+
+  def extend[I1 <: I,O1 >: O]: Pipe[I1,O1,R] =
+    Delay(() => delayed().extend);
 }
 
 
 object Pipe {
-  def apply[I,O,R](result: R): Pipe[I,O,R] = Done(result);
+  def finish[I,O]: Pipe[I,O,Unit] = Done(());
+  def finish[I,O,R](result: => R): Pipe[I,O,R] = Done(result);
 
-  def request[I,O,R]: Pipe[I,O,I] =
+  def delay[I,O,R](p: => Pipe[I,O,R]): Pipe[I,O,R] = Delay(() => p);
+
+  def request[I,O]: Pipe[I,O,I] =
     NeedInput(i => Done(i));
 
-  def respond[I,O,R](o: O): Pipe[I,O,Unit] =
+  def respond[I,O](o: O): Pipe[I,O,Unit] =
     HaveOutput(o, () => { Done(()) })
+
+  def forever[I,O,R](p: Pipe[I,O,_]): Pipe[I,O,R] =
+    p >> forever(p);
 
   /**
    * Composes two pipes, blocked on respond. This means that the second
@@ -54,15 +87,17 @@ object Pipe {
     var i = inp;
     var o = outp;
     while (true) {
-      val consume = outp match {
+      o match {
         case Done(r)              => return Done(r)
-        case HaveOutput(o, next)  => return HaveOutput(o, () => { pipe(inp, next()) } )
-        case NeedInput(f)         => f;
-      }
-      inp match {
-        case HaveOutput(x, next)  => { i = next(); o = consume(x); }
-        case Done(r)              => return Done(r)
-        case NeedInput(c)         => return NeedInput((x: I) => pipe(c(x), outp));
+        case Delay(o1)            => o = o1();
+        case HaveOutput(o, next)  => return HaveOutput(o, () => { pipe(i, next()) } )
+        case NeedInput(consume)   =>
+          i match {
+            case HaveOutput(x, next)  => { i = next(); o = consume(x); }
+            case Delay(i1)            => i = i1();
+            case Done(r)              => return Done(r)
+            case NeedInput(c)         => return NeedInput((x: I) => pipe(c(x), o));
+          }
       }
     }
     // Just to satisfy the compiler, we never get here.
@@ -71,12 +106,14 @@ object Pipe {
 
 
   @tailrec
-  def runPipe[R](pipe: Pipe[Unit,Nothing,R]): R =
+  def runPipe[R](pipe: Pipe[Unit,Nothing,R]): R = {
     pipe match {
       case Done(r)            => r;
+      case Delay(p)           => runPipe(p());
       case HaveOutput(o, _)   => o;
       case NeedInput(consume) => runPipe(consume(()));
     }
+  }
 
   def idP[A,R]: Pipe[A,A,R] =
     NeedInput(x => HaveOutput(x, () => { idP }));

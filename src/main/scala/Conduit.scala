19d31b80296e7dfff0c993a263cd8b7f7e827b38
Petr <petr.mvd@gmail.com>
2013-03-03 10:57:28 +0100

    Experimental branch with co/contra-variant type.

--- src/main/scala/Conduit.scala
+++ src/main/scala/Conduit.scala
@@ -1,87 +1,100 @@
 import annotation.tailrec
 import collection.mutable.{ArrayBuffer, Buffer}
 
-sealed trait Pipe[I,O,R]
+sealed trait Pipe[-I,+O,+R]
 {
+  /*
   def flatMap[R1](f: R => Pipe[I,O,R1]): Pipe[I,O,R1];
   final def >>[R1](p: => Pipe[I,O,R1]): Pipe[I,O,R1] = flatMap(_ => p);
-  def map[R1](f: R => R1): Pipe[I,O,R1];
+  */
+  final def map[R1](f: R => R1): Pipe[I,O,R1] = Pipe.map(this, f);
 
+  /*
   final def >->[X](that: Pipe[O,X,R]) = Pipe.pipe(this, that);
   final def <-<[X](that: Pipe[X,I,R]) = Pipe.pipe(that, this);
+  */
 
-  final def forever[R]: Pipe[I,O,R] = Pipe.forever(this);
+  final def forever[Rx]: Pipe[I,O,Rx] = Pipe.forever(this);
   final def finalizer(fin: => Unit): Pipe[I,O,R] = Pipe.delay(this, fin);
-
-  implicit def extend[I1 <: I,O1 >: O]: Pipe[I1,O1,R];
 }
 
-case class HaveOutput[I,O,R](output: O, next: () => Pipe[I,O,R])
+final case class HaveOutput[-I,+O,+R](output: O, next: () => Pipe[I,O,R])
     extends Pipe[I,O,R]
 {
+/*
   def flatMap[R1](f: R => Pipe[I,O,R1]): Pipe[I,O,R1] =
     HaveOutput(output, () => { next().flatMap(f) });
   def map[R1](f: R => R1): Pipe[I,O,R1] =
     HaveOutput(output, () => { next().map(f) });
-
-  def extend[I1 <: I,O1 >: O]: Pipe[I1,O1,R] =
-    HaveOutput(output, () => { next().extend });
+  */
 }
 
-case class NeedInput[I,O,R](consume: I => Pipe[I,O,R])
+final case class NeedInput[-I,+O,R](consume: I => Pipe[I,O,R])
     extends Pipe[I,O,R]
 {
+  /*
   def flatMap[R1](f: R => Pipe[I,O,R1]): Pipe[I,O,R1] =
     NeedInput(i => consume(i).flatMap(f));
   def map[R1](f: R => R1): Pipe[I,O,R1] =
     NeedInput(i => consume(i).map(f));
-
-  def extend[I1 <: I,O1 >: O]: Pipe[I1,O1,R] =
-    NeedInput(x => { consume(x).extend });
+  */
 }
 
-case class Done[I,O,R](result: R)
+final case class Done[-I,+O,R](result: R)
     extends Pipe[I,O,R]
 {
+  /*
   def flatMap[R1](f: R => Pipe[I,O,R1]): Pipe[I,O,R1] =
     f(result);
   def map[R1](f: R => R1): Pipe[I,O,R1] =
     Done(f(result));
-
-  def extend[I1,O1]: Pipe[I1,O1,R] =
-    Done(result);
+  */
 }
 
-case class Delay[I,O,R](delayed: () => Pipe[I,O,R], finalizer: Option[() => Unit])
+final case class Delay[I,O,R](delayed: () => Pipe[I,O,R], finalizer: Option[() => Unit])
     extends Pipe[I,O,R]
 {
+/*
   def flatMap[R1](f: R => Pipe[I,O,R1]): Pipe[I,O,R1] =
     Delay(() => delayed().flatMap(f), finalizer);
   def map[R1](f: R => R1): Pipe[I,O,R1] =
     Delay(() => delayed().map(f), finalizer);
-
-  def extend[I1 <: I,O1 >: O]: Pipe[I1,O1,R] =
-    Delay(() => delayed().extend, finalizer);
+  */
 }
 
 
 object Pipe {
-  def finish[I,O]: Pipe[I,O,Unit] = Done(());
-  def finish[I,O,R](result: => R): Pipe[I,O,R] = Done(result);
+  def finish: Pipe[Any,Nothing,Unit] = finish(());
+  def finish[R](result: => R): Pipe[Any,Nothing,R] = Done(result);
 
+  def finalizer(finalizer: => Unit): Pipe[Any,Nothing,Unit]
+    = Delay(() => finish, Some(() => finalizer));
   def delay[I,O,R](p: => Pipe[I,O,R]): Pipe[I,O,R]
     = Delay(() => p, None);
   def delay[I,O,R](p: => Pipe[I,O,R], finalizer: => Unit): Pipe[I,O,R]
     = Delay(() => p, Some(() => finalizer));
 
-  def request[I,O]: Pipe[I,O,I] =
+  def request[I]: Pipe[I,Nothing,I] =
     NeedInput(i => Done(i));
 
-  def respond[I,O](o: O): Pipe[I,O,Unit] =
+  def respond[O](o: O): Pipe[Any,O,Unit] =
     HaveOutput(o, () => { Done(()) })
 
+  //@tailrec
+  def flatMap[I,O,Ri,R](p: Pipe[I,O,Ri], f: Ri => Pipe[I,O,R]): Pipe[I,O,R] =
+    p match {
+      case HaveOutput(out, next)      => HaveOutput(out, () => flatMap(next(), f));
+      case NeedInput(consume)         => NeedInput(x => flatMap(consume(x), f));
+      case Delay(delayed, fin)        => Delay(() => flatMap(delayed(), f), fin);
+      case Done(result)               => f(result);
+    }
+  def map[I,O,Ri,R](p: Pipe[I,O,Ri], f: Ri => R): Pipe[I,O,R] =
+    flatMap[I,O,Ri,R](p, x => Done(f(x)));
+  def andThen[I,O,R](p: Pipe[I,O,_], q: => Pipe[I,O,R]): Pipe[I,O,R] =
+    flatMap[I,O,Any,R](p, _ => q);
+
   def forever[I,O,R](p: Pipe[I,O,_]): Pipe[I,O,R] =
-    p >> forever(p);
+    andThen(p, { forever(p) });
 
   /**
    * Composes two pipes, blocked on respond. This means that the second
@@ -123,6 +136,6 @@ object Pipe {
     }
   }
 
-  def idP[A,R]: Pipe[A,A,R] =
+  def idP[A,Any]: Pipe[A,A,Any] =
     NeedInput(x => HaveOutput(x, () => { idP }));
 }
--- src/main/scala/PipeUtil.scala
+++ src/main/scala/PipeUtil.scala
@@ -5,31 +5,30 @@ object PipeUtil extends App {
   import Pipe._;
 
   def filter[A,R](p: A => Boolean): Pipe[A,A,R] =
-    request[A,A].flatMap(x => {
-      if (p(x)) (respond[A,A](x) >> filter(p)) else filter(p);
+    flatMap(request[A], (x: A) => {
+      if (p(x)) andThen(respond[A](x), filter(p)) else filter(p);
     });
 
   def readLines[I](r: BufferedReader): Pipe[I,String,Unit] = {
     def loop: Pipe[I,String,Unit] = r.readLine match {
-        case null   => finish[I,String,Unit]({ r.close(); })
-        case l      => respond(l) >> loop;
+        case null   => finish
+        case l      => andThen(respond(l), loop);
       }
     delay(loop, { r.close(); });
   }
 
   def writeLines[O](w: Writer): Pipe[String,O,Unit] =
-    request[String,O].flatMap(x => { w.write(x); w.write('\n'); writeLines(w); }).finalizer({ w.close(); });
+    flatMap(request[String], (x:String) => { w.write(x); w.write('\n'); writeLines(w); }).finalizer({ w.close(); });
  
 
   def fromIterable[A,I](i: Iterable[A]): Pipe[I,A,Unit] = fromIterator(i.iterator);
   def fromIterator[A,I](i: Iterator[A]): Pipe[I,A,Unit] =
-    if (i.hasNext) { respond(i.next()) >> fromIterator(i); }
+    if (i.hasNext) andThen(respond(i.next()), fromIterator(i));
     else finish();
 
   //def toCol[A,O,C <: Growable[A]](c: C): Pipe[A,O,C] =
  
 
-  /*
   {
     val child = Runtime.getRuntime().exec(Array(
         "/bin/sh", "-c", "find /home/p/projects/sosirecr/ -name '*java' -type f | xargs cat" ));
@@ -38,19 +37,18 @@ object PipeUtil extends App {
 
     val f = filter[String,Unit](s => s.length < 30);
     val o = writeLines[Nothing](new OutputStreamWriter(System.out));
-    runPipe[Unit](pipe[Unit,String,Nothing,Unit](i >-> f, o));
+    runPipe[Unit](pipe[Unit,String,Nothing,Unit](pipe(i, f), o));
 
     System.err.println("Process terminated.");
     child.waitFor();
     System.err.println("Exit status: " + child.exitValue());
     //System.exit(0);
   }
-  */
   {
     val i = fromIterable[String,Unit](List("abc", "efg4", "123"));
     val f = filter[String,Unit](s => s.length <= 3);
     val o = writeLines[Nothing](new OutputStreamWriter(System.out));
-    runPipe[Unit](pipe[Unit,String,Nothing,Unit](i >-> f, o));
+    runPipe[Unit](pipe[Unit,String,Nothing,Unit](pipe(i, f), o));
     System.err.println("Finished.");
     System.exit(0);
   }

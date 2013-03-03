a516fbc30e6eb1545d0c7394719e108931e3dfa0
Petr <petr.mvd@gmail.com>
2013-03-03 11:57:29 +0100

    Improved type signatures in example methods.

--- src/main/scala/PipeUtil.scala
+++ src/main/scala/PipeUtil.scala
@@ -4,27 +4,27 @@ import scala.collection.generic.Growable
 object PipeUtil extends App {
   import Pipe._;
 
-  def filter[A,R](p: A => Boolean): Pipe[A,A,R] =
-    flatMap(request[A], (x: A) => {
-      if (p(x)) andThen(respond[A](x), filter(p)) else filter(p);
+  def filter[A](p: A => Boolean): Pipe[A,A,Nothing] =
+    flatMap(request, (x: A) => {
+      if (p(x)) andThen(respond(x), filter(p)) else filter(p);
     });
 
-  def readLines[I](r: BufferedReader): Pipe[I,String,Unit] = {
-    def loop: Pipe[I,String,Unit] = r.readLine match {
+  def readLines(r: BufferedReader): Pipe[Any,String,Unit] = {
+    def loop: Pipe[Any,String,Unit] = r.readLine match {
         case null   => finish
         case l      => andThen(respond(l), loop);
       }
     delay(loop, { r.close(); });
   }
 
-  def writeLines[O](w: Writer): Pipe[String,O,Unit] =
+  def writeLines(w: Writer): Pipe[String,Nothing,Nothing] =
     flatMap(request[String], (x:String) => { w.write(x); w.write('\n'); writeLines(w); }).finalizer({ w.close(); });
  
 
-  def fromIterable[A,I](i: Iterable[A]): Pipe[I,A,Unit] = fromIterator(i.iterator);
-  def fromIterator[A,I](i: Iterator[A]): Pipe[I,A,Unit] =
+  def fromIterable[A](i: Iterable[A]): Pipe[Any,A,Unit] = fromIterator(i.iterator);
+  def fromIterator[A](i: Iterator[A]): Pipe[Any,A,Unit] =
     if (i.hasNext) andThen(respond(i.next()), fromIterator(i));
-    else finish();
+    else finish;
 
   //def toCol[A,O,C <: Growable[A]](c: C): Pipe[A,O,C] =
  
@@ -33,11 +33,11 @@ object PipeUtil extends App {
     val child = Runtime.getRuntime().exec(Array(
         "/bin/sh", "-c", "find /home/p/projects/sosirecr/ -name '*java' -type f | xargs cat" ));
     val is = child.getInputStream();
-    val i = readLines[Unit](new BufferedReader(new InputStreamReader(is)));
+    val i = readLines(new BufferedReader(new InputStreamReader(is)));
 
-    val f = filter[String,Unit](s => s.length < 30);
-    val o = writeLines[Nothing](new OutputStreamWriter(System.out));
-    runPipe[Unit](pipe[Unit,String,Nothing,Unit](pipe(i, f), o));
+    val f = filter[String](s => s.length < 30);
+    val o = writeLines(new OutputStreamWriter(System.out));
+    runPipe(pipe(pipe(i, f), o));
 
     System.err.println("Process terminated.");
     child.waitFor();
@@ -45,10 +45,10 @@ object PipeUtil extends App {
     //System.exit(0);
   }
   {
-    val i = fromIterable[String,Unit](List("abc", "efg4", "123"));
-    val f = filter[String,Unit](s => s.length <= 3);
-    val o = writeLines[Nothing](new OutputStreamWriter(System.out));
-    runPipe[Unit](pipe[Unit,String,Nothing,Unit](pipe(i, f), o));
+    val i = fromIterable(List("abc", "efg4", "123"));
+    val f = filter[String](s => s.length <= 3);
+    val o = writeLines(new OutputStreamWriter(System.out));
+    runPipe(pipe(pipe(i, f), o));
     System.err.println("Finished.");
     System.exit(0);
   }

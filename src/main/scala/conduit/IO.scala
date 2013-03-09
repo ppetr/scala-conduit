fb99614185eacb142e6429a9b50201ad5a1ac96f
Petr Pudlak <petr.mvd@gmail.com>
2013-03-09 10:51:20 +0100

    Reorganize sources into objects.
    Add a package name.

--- src/main/scala/conduit/IO.scala
+++ src/main/scala/conduit/IO.scala
@@ -1,38 +1,20 @@
-import java.io._
-import scala.collection.generic.Growable
+package conduit
 
-object PipeUtil {
+import java.io._
+
+object IO {
   import Pipe._;
+  import Util._;
 
   implicit def closeFin(implicit c: Closeable): Finalizer =
     Finalizer { System.err.println("Closing " + c); c.close() }
-
-  implicit def implicitAsPipe[O](value: O) = new {
-    def asPipe: Pipe[Any,O,Unit] = respond[O](value)(Finalizer.empty);
-  }
-
-  def filter[A](p: A => Boolean): Pipe[A,A,Nothing] = {
-    import Finalizer.empty
-    def loop: Pipe[A,A,Nothing] =
-      request[A].asO[A].flatMap(x => if (p(x)) (respond(x).as[A,A] >> loop) else loop);
-    loop;
-  }
-  /*
-  (for(x <- request[A].asO[A];
-      _ <- if (p(x)) respond(x).asI[A] else finish.as[A,A]
-    ) yield finish.as[A,A]).forever;
-  */
-  /*
-    flatMap(request, (x: A) => {
-      if (p(x)) (respond(x) >> filter(p)) else filter(p);
-    });
-  */
 
   lazy val readLinesFile: Pipe[File,String,Nothing] = {
     import Finalizer.empty
     pipe(mapP((f: File) => new BufferedReader(new InputStreamReader(new FileInputStream(f), "UTF-8"))), readLines);
   }
     //arrP((f: File) => new BufferedReader(new InputStreamReader(new FileInputStream(f), "UTF-8"))) >-> readLines;
+
   lazy val readLines: Pipe[BufferedReader,String,Nothing] =
     unfold[BufferedReader,String,Unit](readLines _);
 
@@ -41,40 +23,14 @@ object PipeUtil {
     untilF[Any,String](Option(r.readLine).map(respond[String] _));
   }
 
+  implicit def bufferedReaderToSource(r: BufferedReader) = new SourceLike[String] {
+    override def toSource = readLines(r);
+  }
+
+
   def writeLines(w: Writer): Pipe[String,Nothing,Nothing] = {
     implicit val fin = closeFin(w)
     forever(request((x: String) => { w.write(x); w.write('\n'); w.flush(); finish; }));
-  }
- 
-
-  def fromSeq[A](values: A*): Pipe[Any,A,Unit] = fromIterable(values);
-  def fromIterable[A](i : Iterable[A]): Pipe[Any,A,Unit]
-    = fromIterator(i.iterator);
-  def fromIterator[A](i : Iterator[A]): Pipe[Any,A,Unit] = {
-    import Finalizer.empty
-    untilF(if (i.hasNext) Some(respond[A](i.next())) else None);
-  }
-
-  def fromIterable[A]: Pipe[Iterable[A],A,Nothing] =
-    unfold[Iterable[A],A,Unit](i => fromIterable(i));
-  def fromIterator[A]: Pipe[Iterator[A],A,Nothing] =
-    unfold[Iterator[A],A,Unit](i => fromIterator(i));
-
-  //def toCol[A,O,C <: Growable[A]](c: C): Pipe[A,O,C] =
-
-
-  trait SourceLike[+O] {
-    def toSource: Pipe[Any,O,Unit];
-  }
-  trait SinkLike[-I] {
-    def toSink: Pipe[I,Nothing,Nothing];
-  }
-
-  implicit def iterableToSource[A](it: Iterable[A]) = new SourceLike[A] {
-    override def toSource = fromIterable(it);
-  }
-  implicit def bufferedReaderToSource(r: BufferedReader) = new SourceLike[String] {
-    override def toSource = readLines(r);
   }
  
 
---
+++ src/main/scala/conduit/Util.scala
@@ -0,0 +1,46 @@
+package conduit
+
+import scala.collection.generic.Growable
+
+object Util {
+  import Pipe._;
+
+  implicit def implicitAsPipe[O](value: O) = new {
+    def asPipe: Pipe[Any,O,Unit] = respond[O](value)(Finalizer.empty);
+  }
+
+  def filter[A](p: A => Boolean): Pipe[A,A,Nothing] = {
+    import Finalizer.empty
+    def loop: Pipe[A,A,Nothing] =
+      request[A].asO[A].flatMap(x => if (p(x)) (respond(x).as[A,A] >> loop) else loop);
+    loop;
+  }
+ 
+
+  def fromSeq[A](values: A*): Pipe[Any,A,Unit] = fromIterable(values);
+  def fromIterable[A](i : Iterable[A]): Pipe[Any,A,Unit]
+    = fromIterator(i.iterator);
+  def fromIterator[A](i : Iterator[A]): Pipe[Any,A,Unit] = {
+    import Finalizer.empty
+    untilF(if (i.hasNext) Some(respond[A](i.next())) else None);
+  }
+
+  def fromIterable[A]: Pipe[Iterable[A],A,Nothing] =
+    unfold[Iterable[A],A,Unit](i => fromIterable(i));
+  def fromIterator[A]: Pipe[Iterator[A],A,Nothing] =
+    unfold[Iterator[A],A,Unit](i => fromIterator(i));
+
+  //def toCol[A,O,C <: Growable[A]](c: C): Pipe[A,O,C] =
+
+
+  trait SourceLike[+O] {
+    def toSource: Pipe[Any,O,Unit];
+  }
+  trait SinkLike[-I] {
+    def toSink: Pipe[I,Nothing,Nothing];
+  }
+
+  implicit def iterableToSource[A](it: Iterable[A]) = new SourceLike[A] {
+    override def toSource = fromIterable(it);
+  }
+}
